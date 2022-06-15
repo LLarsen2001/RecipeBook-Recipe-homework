92cbfa71a4a56f4accf6231fa4335b0f72044366
Lance Larsen <lancelarsen2001@gmail.com>
2022-06-15 16:58:51 -0600

    commit

---
+++ client/src/providers/DataProvider.js
@@ -0,0 +1,45 @@
+import React from "react";
+import axios from "axios";
+
+export const DataContext = React.createContext();
+
+const DataProvider = (props) => {
+    const [recipebooks, setRecipebooks] = useState([])
+
+    useEffect(() => {
+        getRecipebooks();
+    }, []);
+
+    const getRecipebooks = () => {
+        try {
+            let res = await axios.get('/api/recipebooks')
+            setRecipebooks(res.data)
+        }
+        catch (err) {
+            alert('error has occured in the setRecipebooks')
+        }
+    }
+
+    const updateRecipebook = async (recipebook) => {
+        if (recipebook.title === "" || !recipebook.id) {
+            alert('bad Book name')
+            return;
+        } try {
+            let res = await axios.put(`/api/recipebooks/${recipebook.id}`, recipebook);
+            let updatedRecipebook = recipebook.map((a) => a.id === res.data.id ? res.data : a)
+            setRecipebooks(updatedRecipebook)
+        } catch (err) {
+            alert('error has occured in the updateRecipebook')
+        }
+
+    }
+
+    return (
+        <DataContext.Provider value={{ getRecipebooks, updateRecipebook }}>
+            {props.children}
+        </DataContext.Provider>
+    );
+
+    export default DataProvider;
+
+
